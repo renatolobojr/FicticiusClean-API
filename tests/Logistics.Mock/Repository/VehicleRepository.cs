4a7679adbefccca9bb8b779cec50cf63bb6822b4
renatolobojr <renatolobojr@gmail.com>
2022-05-23 06:19:39 -0300

    updated license Plate mocking

--- tests/Logistics.Mock/Repository/VehicleRepository.cs
+++ tests/Logistics.Mock/Repository/VehicleRepository.cs
@@ -28,12 +28,12 @@ namespace Logistics.Mock.Repository
 
         private static List<Vehicle> _vehiclesMock = new()
         {
-            new Vehicle() { Guid = Guid.NewGuid() ,Name = "Fusca Azul" , LicensePlate = "g" , Make = "Volkswagen" , Model = "Fusca" , ModelYear = 76, ProductionDate = 76, FuelConsumptionCity = 8 , FuelConsumptionRoad = 8},
-            new Vehicle() { Guid = Guid.NewGuid() ,Name = "Corsa Preto" , LicensePlate = "g" , Make = "Chevrolet" , Model = "Corsa Classic 1.0 Flexpower 4P" , ModelYear = 13, ProductionDate = 12, FuelConsumptionCity = 7.4 , FuelConsumptionRoad = 9.2},
-            new Vehicle() { Guid = Guid.NewGuid() ,Name = "Corola Prata" , LicensePlate = "g" , Make = "Toyota" , Model = "Corola GLi 2.0 16V Flex" , ModelYear = 20, ProductionDate = 19, FuelConsumptionCity = 11.4 , FuelConsumptionRoad = 13.2},
-            new Vehicle() { Guid = Guid.NewGuid() ,Name = "Ford Ka Antigo" , LicensePlate = "g" , Make = "Ford" , Model = "Ka CLX 1.3i 3p" , ModelYear = 97, ProductionDate = 97, FuelConsumptionCity = 12.7 , FuelConsumptionRoad = 16.8},
-            new Vehicle() { Guid = Guid.NewGuid() ,Name = "HB20 Novo" , LicensePlate = "g" , Make = "Hyundai" , Model = "HB20 Evolution 1.0 Turbo Flex 12V Automático" , ModelYear = 22, ProductionDate = 22, FuelConsumptionCity = 12.8 , FuelConsumptionRoad = 14.6},
-            new Vehicle() { Guid = Guid.NewGuid() ,Name = "Uno branco" , LicensePlate = "g" , Make = "Fiat" , Model = "Uno Mille Way Economy 1.0 Fire Flex 4p " , ModelYear = 11, ProductionDate = 11, FuelConsumptionCity = 11.1 , FuelConsumptionRoad = 15.6}
+            new Vehicle() { Guid = Guid.NewGuid() ,Name = "Fusca Azul" , LicensePlate = "GQV-7845" , Make = "Volkswagen" , Model = "Fusca" , ModelYear = 76, ProductionDate = 76, FuelConsumptionCity = 8 , FuelConsumptionRoad = 8},
+            new Vehicle() { Guid = Guid.NewGuid() ,Name = "Corsa Preto" , LicensePlate = "KRM-9024" , Make = "Chevrolet" , Model = "Corsa Classic 1.0 Flexpower 4P" , ModelYear = 13, ProductionDate = 12, FuelConsumptionCity = 7.4 , FuelConsumptionRoad = 9.2},
+            new Vehicle() { Guid = Guid.NewGuid() ,Name = "Corola Prata" , LicensePlate = "OLX-8479" , Make = "Toyota" , Model = "Corola GLi 2.0 16V Flex" , ModelYear = 20, ProductionDate = 19, FuelConsumptionCity = 11.4 , FuelConsumptionRoad = 13.2},
+            new Vehicle() { Guid = Guid.NewGuid() ,Name = "Ford Ka Antigo" , LicensePlate = "LHJ-2845" , Make = "Ford" , Model = "Ka CLX 1.3i 3p" , ModelYear = 97, ProductionDate = 97, FuelConsumptionCity = 12.7 , FuelConsumptionRoad = 16.8},
+            new Vehicle() { Guid = Guid.NewGuid() ,Name = "HB20 Novo" , LicensePlate = "GNK-9234" , Make = "Hyundai" , Model = "HB20 Evolution 1.0 Turbo Flex 12V Automático" , ModelYear = 22, ProductionDate = 22, FuelConsumptionCity = 12.8 , FuelConsumptionRoad = 14.6},
+            new Vehicle() { Guid = Guid.NewGuid() ,Name = "Uno branco" , LicensePlate = "ORM-9583" , Make = "Fiat" , Model = "Uno Mille Way Economy 1.0 Fire Flex 4p " , ModelYear = 11, ProductionDate = 11, FuelConsumptionCity = 11.1 , FuelConsumptionRoad = 15.6}
         };
 
     }
